343a6dfb0c309243fbae7b67a9d4baab8d8bdca8
Dmytro Parfeniuk <parfeniukinik@gmail.com>
2021-11-05 16:39:37 +0200

    Update urls file

--- src/config/urls.py
+++ src/config/urls.py
@@ -20,11 +20,10 @@ urlpatterns = [
 if settings.DEBUG:
     # import debug_toolbar
     from django.conf.urls.static import static
-    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
     # from config.yasg import urlpatterns as yasg_docs
 
-    urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     # urlpatterns += [path("__debug__/", include(debug_toolbar.urls))]
     # urlpatterns += yasg_docs
